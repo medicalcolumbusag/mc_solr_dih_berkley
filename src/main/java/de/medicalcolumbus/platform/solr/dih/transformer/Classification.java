cf569f01c5e0c76c4431aa0b6334821205781afe
chromyd <dusan.chromy@medicalcolumbus.de>
2016-02-09 11:45:11 +0100

    MP-1551 Added JavaDoc

--- src/main/java/de/medicalcolumbus/platform/solr/dih/transformer/Classification.java
+++ src/main/java/de/medicalcolumbus/platform/solr/dih/transformer/Classification.java
@@ -4,6 +4,42 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * This transformer creates composed qualification names for every parent
+ * and add all the parents in different facet values.
+ *
+ * Example:
+ * Product classified as:
+ * <ul>
+ * <li>Ward Supplies
+ *     <ul>
+ *         <li>Diagnostic appliances
+ *             <ul>
+ *                 <li>Blood pressure measuring devices
+ *                     <ul>
+ *                         <li> / parts
+ *                             <ul>
+ *                                 <li> / connecting pieces and tubes</li>
+ *                             </ul>
+ *                         </li>
+ *                     </ul>
+ *                 </li>
+ *             </ul>
+ *         </li>
+ *     </ul>
+ * </li>
+ * </ul>
+ * Will result in the following Solr field:
+ * <pre><code>
+    "classification_101_en": [
+	    "1_Ward Supplies",
+	    "2_Ward Supplies, Diagnostic appliances",
+	    "3_Ward Supplies, Diagnostic appliances, Blood pressure measuring devices",
+	    "4_Ward Supplies, Diagnostic appliances, Blood pressure measuring devices, / parts",
+	    "5_Ward Supplies, Diagnostic appliances, Blood pressure measuring devices, / parts, / connecting pieces and tubes"
+    ]
+ </code></pre>
+ */
 public class Classification {
 	public Object transformRow(Map<String, Object> row) {
 		if (row.get("FULL_NAME") == null) {
