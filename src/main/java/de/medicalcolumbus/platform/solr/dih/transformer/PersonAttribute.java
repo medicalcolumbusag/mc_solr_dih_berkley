d29d883a9970f4043624ee726736d13a1b5abf21
Alexandru Ionescu <alexandru.ionescu@rinftech.com>
2018-01-31 17:39:12 +0200

    GM-370 Add transformer for Person Attribute and update Organization Attribute with support for date field

--- src/main/java/de/medicalcolumbus/platform/solr/dih/transformer/PersonAttribute.java
+++ src/main/java/de/medicalcolumbus/platform/solr/dih/transformer/PersonAttribute.java
@@ -2,31 +2,24 @@ package de.medicalcolumbus.platform.solr.dih.transformer;
 
 import java.util.Map;
 
-public class OrganizationAttribute {
+public class PersonAttribute {
 
 	public Object transformRow(Map<String, Object> row) {
 		String type = row.get("KIND") == null ? "" : row.get("KIND").toString();
-		String name = row.get("NAME").toString() + getFieldExtensionByType(type.toLowerCase());
+		String name = row.get("ATTRIBUTE_NAME").toString() + getFieldExtensionByType(type.toLowerCase());
 		row.put(name, row.get("VALUE"));
 
-		if ("customer_type_ss".equals(name)) {
-			row.put("_intrinsic_customer_type_ss", row.get("VALUE"));
-		}
 		return row;
 	}
 
 	private String getFieldExtensionByType(String type) {
 		switch (type) {
-			case "integer":
-				return "_ti";
 			case "text":
 				return "_ss";
-			case "decimal":
-				return "_td";
-			case "boolean":
-				return "_b";
 			case "date":
 				return "_dt";
+			case "boolean":
+				return "_b";
 			default:
 				return "";
 		}
