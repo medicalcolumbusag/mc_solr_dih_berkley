0fe8cf5166c3210866cda75721b3310f53304e95
mariuschiroiu <marius.chiroiu@rinftech.com>
2018-02-26 15:14:52 +0200

    MO-2906 added translation for productStatus

---
+++ src/main/java/de/medicalcolumbus/platform/solr/dih/transformer/ProductStatus.java
@@ -0,0 +1,12 @@
+package de.medicalcolumbus.platform.solr.dih.transformer;
+
+public class ProductStatus extends Translation {
+
+	private static final String PRODUCT_STATUS_TXT = "product_status_";
+
+	@Override
+	protected String getTranslation() {
+		return PRODUCT_STATUS_TXT;
+	}
+
+}
