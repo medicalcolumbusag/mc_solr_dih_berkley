04533a96b6f4bfce2d89f16d33852633f4f1adac
madalinavrinceanu <madalinavrinceanu92@gmail.com>
2018-05-22 11:36:26 +0300

    MP-2800 Adapted OrganizationAttribute transformer.

--- src/main/java/de/medicalcolumbus/platform/solr/dih/transformer/OrganizationAttribute.java
+++ src/main/java/de/medicalcolumbus/platform/solr/dih/transformer/OrganizationAttribute.java
@@ -5,30 +5,13 @@ import java.util.Map;
 public class OrganizationAttribute {
 
 	public Object transformRow(Map<String, Object> row) {
-		String type = row.get("KIND") == null ? "" : row.get("KIND").toString();
-		String name = row.get("NAME").toString() + getFieldExtensionByType(type.toLowerCase());
-		row.put(name, row.get("VALUE"));
+		String type = row.get("indexing_type").toString().trim();
+		String name = row.get("attribute_code").toString().trim() + type;
+		row.put(name, row.get("attribute_value").toString().trim());
 
 		if ("customer_type_ss".equals(name)) {
-			row.put("_intrinsic_customer_type_ss", row.get("VALUE"));
+			row.put("_intrinsic_customer_type_ss", row.get("attribute_value").toString().trim());
 		}
 		return row;
-	}
-
-	private String getFieldExtensionByType(String type) {
-		switch (type) {
-			case "integer":
-				return "_ti";
-			case "text":
-				return "_ss";
-			case "decimal":
-				return "_td";
-			case "boolean":
-				return "_b";
-			case "date":
-				return "_dt";
-			default:
-				return "";
-		}
 	}
 }
