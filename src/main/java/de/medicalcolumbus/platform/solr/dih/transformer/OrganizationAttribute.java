c7434dc38e75533ba10ed99ade4e31bd19d2c54d
madalinavrinceanu <madalinavrinceanu92@gmail.com>
2018-05-24 18:04:58 +0300

    MP-2800 Review updates.

--- src/main/java/de/medicalcolumbus/platform/solr/dih/transformer/OrganizationAttribute.java
+++ src/main/java/de/medicalcolumbus/platform/solr/dih/transformer/OrganizationAttribute.java
@@ -7,11 +7,8 @@ public class OrganizationAttribute {
 	public Object transformRow(Map<String, Object> row) {
 		String type = row.get("indexing_type").toString().trim();
 		String name = row.get("attribute_code").toString().trim() + type;
-		row.put(name, row.get("attribute_value").toString().trim());
+		row.put(name, row.get("attribute_value").toString());
 
-		if ("customer_type_ss".equals(name)) {
-			row.put("_intrinsic_customer_type_ss", row.get("attribute_value").toString().trim());
-		}
 		return row;
 	}
 }
