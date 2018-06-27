4d1aee4f448e5b009d4947574220aaa941708e5c
madalinavrinceanu <madalinavrinceanu92@gmail.com>
2018-06-27 14:49:20 +0300

    MP-3115 Back to upper case because of H2 incompatibility.

--- src/main/java/de/medicalcolumbus/platform/solr/dih/transformer/OrganizationAttribute.java
+++ src/main/java/de/medicalcolumbus/platform/solr/dih/transformer/OrganizationAttribute.java
@@ -5,9 +5,9 @@ import java.util.Map;
 public class OrganizationAttribute {
 
 	public Object transformRow(Map<String, Object> row) {
-		String type = row.get("indexing_type").toString().trim();
-		String name = row.get("attribute_code").toString().trim() + type;
-		row.put(name, row.get("attribute_value").toString());
+		String type = row.get("INDEXING_TYPE").toString().trim();
+		String name = row.get("ATTRIBUTE_CODE").toString().trim() + type;
+		row.put(name, row.get("ATTRIBUTE_VALUE").toString());
 
 		return row;
 	}
