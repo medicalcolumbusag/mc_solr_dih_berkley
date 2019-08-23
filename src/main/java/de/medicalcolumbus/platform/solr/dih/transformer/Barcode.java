adeb644b8008e6c7ad4f009516fe84474e443505
lmuthuraman <lmuthuraman@ghx.com>
2019-08-23 12:16:18 +0530

    GCPP-163 solr query new field added in transformer class

--- src/main/java/de/medicalcolumbus/platform/solr/dih/transformer/Barcode.java
+++ src/main/java/de/medicalcolumbus/platform/solr/dih/transformer/Barcode.java
@@ -7,9 +7,13 @@ public class Barcode {
 
 	private static final String BARCODES_FIELD = "barcodes";
 	private static final String BARCODE_VALUE = "BARCODE";
+	private static final String CODE = "CODE";
 
 	public Object transformRow(Map<String, Object> row) {
 		row.put(getBarcodeFieldName() + "_ss", row.get(BARCODE_VALUE));
+		if(row.get(CODE) != null) {
+            row.put( row.get(CODE).toString().toLowerCase()+ "_" + BARCODES_FIELD  + "_ss", row.get(BARCODE_VALUE));
+        }
 		return row;
 	}
 
